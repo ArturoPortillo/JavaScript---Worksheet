af58183fee348f20ef6f2d1d2e18e2a36ffa29b5
Arturo <Arturojportillo@gmail.com>
2022-08-05 18:28:37 -0300

    Arrays, Clases y Constructor

--- javascript.js
+++ javascript.js
@@ -1,60 +1,45 @@
-            
-            document.getElementById("btn").onclick = function() {myFunction()};
-
-            function myFunction() {
-
-                
-                let expense = 0;
-                let total = 0;
-                
-                    function dec(income, expense) {
-                        return total =  income - expense;
-                    }
-                
-                
-                for (let income = 0; income < 1 ; income--) {
-                    let income = parseInt(prompt("Total de ingresos disponibles: "));
-                            if (isNaN(income)) {
-                                console.log("Ingresa un monto válido.");
-                            break;
-                            } else {
-                                console.log("Tienes " + income + " pesos disponibles.");
-                                }
-                
-                    let expense = parseInt(prompt("Ingresa un gasto: "));
-                        if (isNaN(expense) || expense > income) {
-                            console.log("Ingresa un monto válido.");
-                        break;
-                        }
-                        else {
-                            console.log("Gastas " + expense + " pesos");
-                            }   
-                                        
-                    dec(income,expense);
-                        console.log("Te quedan " + total + " pesos disponibles.")
-                            if (total >= 1) {     
-                            continue;
-                            }
-                            else if (total < 1) 
-                            console.log("No tienes dinero disponible.");  
-                            break;            
-                            } ;
-
-}
-
-document.getElementById("boton").onclick = function() {fun()};
-
+/* document.getElementById("boton").onclick = function() {fun()};
+*
             function fun() {
+ */
 
-                let income = parseInt(prompt("Total de ingresos disponibles: "));
+                /* Creacion de clase con las propiedades que describen a un usuario */
+                class newUser {
+                    constructor(name, lastName, income) {
+                        this.name  = name.toUpperCase();
+                        this.lastName  = lastName.toUpperCase();
+                        this.income = income;
+                        }
+                }
+
+                /* Creacion de un Array donde cargamos los datos del usuario usando nuestra clase "newUser"*/
+
+                const userName = [];
+                userName.push(new newUser((prompt("Ingresa tu nombre: ")), (prompt("Ingresa tu apellido: ")), (parseFloat(prompt("Total de ingresos disponibles: ")))));
+
+                console.log(userName)
+
+                /* El array pasaria a ser userName = [name: ,lastName: , income: ] */
+
+                /* Resultado esperado:
+                newUser
+                name: "ARTURO"
+                lastName: "PORTILLO"
+                income: 50000 ó el monto que ingresemos en el prompt
+                */
+
+                /* Declaramos variables */
+
+                let income = userName[0].income; /* Tomamos el valor de la propiedad "income" dentro del arreglo userName recorriendo el array con [0] */
                 let expense = 0;
                 let total = 0;
                 
-                    function dec(income, expense) {
+                    function dec(income, expense) { 
                         return total =  income - expense;
                     }
-                
-                
+                    
+                /* Luego nuestro ciclo se ejecuta... */
+                                
                 for (let i = 0; i < 1 ; i--) {
                     
                             if (isNaN(income)) {
@@ -64,7 +49,7 @@ document.getElementById("boton").onclick = function() {fun()};
                                 console.log("Tienes " + income + " pesos disponibles.");
                                 }
                 
-                    let expense = parseInt(prompt("Ingresa un gasto: "));
+                    let expense = parseFloat(prompt("Ingresa un gasto: "));
                         if (isNaN(expense) || expense > income) {
                             console.log("Ingresa un monto válido.");
                         break;
@@ -75,13 +60,12 @@ document.getElementById("boton").onclick = function() {fun()};
                                         
                     dec(income,expense);
                         console.log("Te quedan " + total + " pesos disponibles.")
-                            if (total >= 1) {     
+                            if (total >= 1) {   
+                            income = income - expense;  
                             continue;
                             }
                             else if (total < 1) 
                             console.log("No tienes dinero disponible.");  
                             break;            
-                            } ;
-
-}
+                            };                        
 
