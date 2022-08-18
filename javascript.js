c425178056e3217d0dbd2bc507ada2a424bda68b
Arturo <Arturojportillo@gmail.com>
2022-08-17 21:03:28 -0300

    added Events

--- javascript.js
+++ javascript.js
@@ -1,18 +1,21 @@
+/* ################## Create Element Function ################## */
 
-/* Por los momentos me estaria complicando más hacer el proyecto con una clase, si quiero evaluar lo que ingresa el usuario para saber si en el nombre pone numeros y letras se me hace mas facil trabajar con un objeto y luego cargar ese objeto en un array por lo cual 'comento' el codigo de 'Class' */
-/* class User {
-    constructor(userName, lastName, income) {
-        this.userName  = userName.toUpperCase();
-        this.lastName  = lastName.toUpperCase();
-        this.income = income;
-        }
-    } */
+    function elementFromHtml(html) {
+        const template = document.createElement("template");
+
+        template.innerHTML = html.trim();
+
+        return template.content.firstElementChild;
+    }
+
+
+/* ################## Base Code ################## */
 
     const userData = []; 
 
     let newName = prompt("Ingresa tu nombre: ");
-    let userlastName = prompt("Ingresa tu apellido: ");
-    let userIncome = parseFloat(prompt("Total de ingresos disponibles: "));
+    let userlastName = prompt("Ingresa tu apellido: "); 
+    let userIncome = 0; 
     
     const validText = !/[^a-zA-Z]/.test(newName)    
     const validText2 = !/[^a-zA-Z]/.test(userlastName)
@@ -28,6 +31,18 @@ userData.push(newUser)
     console.log(userData)
     console.log("Usuarios registrados: "+userData.length)
 
+/* ################## JSON Tests ################## */
+
+const userValues = JSON.stringify(userData)
+console.log("los valores son"+userValues)
+
+sessionStorage.setItem('User',userValues);
+
+const savedUser = sessionStorage.getItem('User', userValues)
+console.log(savedUser)
+
+/* ################## Validate Input ################## */
+
     if (!/[^a-zA-Z]+$/.test(newName) && !/[^a-zA-Z]+$/.test(userlastName) === true && !/[^\d+$]/.test(userIncome)) {
         /* Validamos que el nombre, el usuario y los ingresos sean validos con RegEx, todas deben ser True para poder continuar */
         console.log("Validando nombre de usuario..."+validText) 
@@ -39,7 +54,6 @@ userData.push(newUser)
         userData.splice(0,2)
         console.log("Datos ingresados inválidos, repita operación.") 
     }
-                                
 
 /* El array pasaria a ser userName = [userName: ,lastName: , income: ] */
 /* Resultado esperado:
@@ -49,7 +63,7 @@ userData.push(newUser)
     income: 50000 ó el monto que ingresemos en el prompt
 */
 
-/* Declaramos variables */
+/* ################## Base vars ################## */
 
     let income = userData[0].income; /* Tomamos el valor de la propiedad "income" dentro del arreglo userData recorriendo el array con [0] */ 
     let expense = 0;
@@ -59,7 +73,7 @@ userData.push(newUser)
             return income - expense;
             }
 
-/* Luego nuestro ciclo se ejecuta... */
+/* ################## Main Loop ################## */
 
 while (total >= 0 && !/[^a-zA-Z]+$/.test(newName) && !/[^a-zA-Z]+$/.test(userlastName) === true && !/[^\d+$]/.test(userIncome)) {
         
@@ -70,7 +84,7 @@ while (total >= 0 && !/[^a-zA-Z]+$/.test(newName) && !/[^a-zA-Z]+$/.test(userlas
             console.log("Tienes " + income + " pesos disponibles.");
         }
         
-        expense = parseFloat(prompt("Ingresa un gasto: "));
+        expense = parseFloat(prompt("Ingresa un gasto: "));        
     if (isNaN(expense) || expense > income) {
         console.log("Ingresa un monto válido.");
         break;
@@ -90,29 +104,80 @@ while (total >= 0 && !/[^a-zA-Z]+$/.test(newName) && !/[^a-zA-Z]+$/.test(userlas
             console.log("No tienes dinero disponible.")  
         break;
         }         
-}
+} 
+/* ################## Main selectors ################## */
 
+const displayUser = document.querySelector(".userData");
+const displayExpense = document.querySelector("#expenseLog");
+const displayBalance = document.querySelector(".currentBalance");
 
-let displayUser = document.querySelector(".userData");
-let displayExpense = document.querySelector(".expense");
-let balance = document.querySelector(".currentBalance");
-
-displayUser.innerText = newName+" "+userlastName;
-displayExpense.innerText = expense;
+displayUser.innerText = "Hola! "+newName+" "+userlastName;
 
 function addBalance() {
     userIncome = parseFloat(prompt("Total de ingresos disponibles: ")) 
-    balance.innerText = userIncome;
+    displayBalance.innerText = userIncome;
 } 
 
-function addExpense() {
-    expense = parseFloat(prompt("Ingresa un gasto: "));
-    displayExpense.innerText = expense;
-    balance.innerText = userIncome - expense;
-} 
+/* ################## Expense Functions ################## */
+
+let aExpense = 0;
+function addExpense() {    
+    aExpense = parseFloat(prompt("Ingresa un gasto: "));
+    let newExpense = document.getElementById('expenseLog')
+    let someExpense = document.createElement("div");
+    someExpense.innerHTML = '<h1 style="color:red;margin-right:4rem"> -$'+aExpense+'</h1>';
+    displayBalance.innerText = userIncome - aExpense;
+    newExpense.appendChild(someExpense)
+    console.log(someExpense)
+}
+displayExpense.innerText = "-$"+aExpense;
+displayBalance.innerText = userIncome - aExpense;
+
+/* ################## User Functions ################## */
 
 function changeUser() {
     let newName = prompt("Ingresa tu nombre: ")
     let userlastName = prompt("Ingresa tu apellido: ")
-    displayUser.innerText = newName+" "+userlastName;
+    let yourInfo = document.querySelector("intro")
+    let yourName = document.createElement("h1");
+    yourName.innerHTML = '<h1 style="color:red;margin-right:4rem"> Hola!'+newName+' '+userlastName+'</h1>';
+    intro.appendChild(yourName)
+    /* displayUser.innerText = "Hola! "+newName+" "+userlastName; */
 } 
+
+let userForm = document.getElementById("form");
+userForm.addEventListener("submit", updateUsername);
+
+function updateUsername(e){
+    e.preventDefault();
+    let formData = e.target
+    displayUser.innerText = "Hola! "+formData.children[2].value+" "+formData.children[4].value;
+}
+/* ################## Balance Functions ################## */
+let balanceInput = document.getElementById("updateBalance");
+balanceInput.addEventListener("submit", sumBalance);
+
+function sumBalance(e){
+    e.preventDefault();
+    let currentBalance = e.target
+    displayBalance.innerText = "$"+currentBalance.children[0].value;
+}
+
+
+
+/* ################## TESTS ################## */
+
+
+/* function addBalance() { 
+    const totalIncome = [];
+    console.log(totalIncome)
+    let userIncome = parseFloat(prompt("Ingresa un nuevo monto: "))
+    totalIncome.push(userIncome)
+    console.log(totalIncome)
+    const actualIncome = totalIncome.reduce((a, b) => a + b, 0)    
+    console.log(actualIncome);
+    
+    displayBalance.innerText = "$ "+actualIncome; 
+}  */
+
+
